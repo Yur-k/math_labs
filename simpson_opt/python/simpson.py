41e747b6f161bc0b4bd1df2b8048663c8c6a7dc0
Yurii Ryzhuk <yura.ryzhuk@gmail.com>
2022-04-09 12:05:51 +0300

    Updated python file

--- simpson_opt/python/simpson.py
+++ simpson_opt/python/simpson.py
@@ -1,8 +1,4 @@
-from distutils.command.build_scripts import first_line_re
 import numpy as np
-
-
-
 
 def Simpson(function,lim1,lim2,n):
     # function - підінтегральна функція
@@ -10,33 +6,13 @@ def Simpson(function,lim1,lim2,n):
     # n - кількість кроків
     # обчислимо крок інтегрування
     h=(lim2-lim1)/n
-    print("h ============= ",h)
     # Підсумовуємо всі значення функції множені на відповідні коефіцієнти
     # та множимо суму на крок
-    first = function(lim1)+function(lim2)
-    print("firstPart ============= ", first)
-    pair = [i for i in range(2, n, 2)]
-    unpair = [i for i in range(1, n, 2)]
-    print("Pair ============= ", pair)
-    print("UnPair ============= ", unpair)
-
-    secondV = [function(lim1+h*i) for i in pair]
-    thirdV = [function(lim1+h*i) for i in unpair]
-    print("pairIndexValue ============= ", secondV)
-    print("unpairIndexValue ============= ", thirdV)
-    second = np.sum(np.asarray([function(lim1+h*i) for i in range(2,n,2)]))*2
-    third = np.sum(np.asarray([function(lim1+h*i) for i in range(1,n,2)]))*4
-    print("thirdPart ============= ", third)
-
-    # print("++++++++++++=== ", np.asarray([i for i in range(2,n,2)]))
-    # print("Range pair ", np.asarray([function(lim1+h*i) for i in range(2,n,2)]))
-    # print("Range unpair ", np.asarray([function(lim1+h*i) for i in range(1,n,2)]))
-
-    print(first,second, third)
-    
-    Int=h/3*(first + second + third)
+    Int=h/3*(function(lim1)+function(lim2)+
+             np.sum(np.asarray([function(lim1+h*i) for i in range(1,n,2)]))*4+
+             np.sum(np.asarray([function(lim1+h*i) for i in range(2,n,2)]))*2
+           )
     return ('Simpson rule', Int)
-
 
 xlim1=2
 xlim2=4
@@ -44,9 +20,4 @@ Ntochok=1000
 sim = Simpson(lambda x: np.exp(x)*np.sin((3*x**2)-x)+np.log(x+2), xlim1, xlim2, Ntochok)
 print(sim)
 
-#print(timeit.timeit(s), number= 2)
 
-
-#timeit.timeit(stmt=s, number=100000)
-
-#print(timeit.timeit(test), number= 2)
